e19a5ed31bab66acb59cc32d35ec006ff35cadff
gurug-prog <ivanmysterys@gmail.com>
2022-07-05 02:54:28 +0300

    fix: eliminate conditional bug

--- ddl/CreateDb.sql
+++ ddl/CreateDb.sql
@@ -1,7 +1,7 @@
 -- use if exists or create mytestdb
 USE master;
 DECLARE @dbName nvarchar = N'mytestdb';
-IF DB_ID(@dbName) IS NOT NULL
+IF DB_ID(@dbName) IS NULL
 CREATE DATABASE mytestdb;
 GO
 USE mytestdb;
